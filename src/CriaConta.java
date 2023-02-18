79dbc6cf98888c9ae973440f97cc2b513d3179cf
Paulojoserc <pjrc83@gmail.com>
2023-02-18 10:42:39 -0300

    Feat - Segunda Instância

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -5,5 +5,15 @@ public static void main(String[] args) {
 	primeiraConta.saldo = 200;
 	
 	System.out.println(primeiraConta.saldo);
+	
+	primeiraConta.saldo +=100;
+	
+	System.out.println(primeiraConta.saldo);
+	
+	Conta segundaConta = new Conta();
+    segundaConta.saldo = 50;
+
+    System.out.println("primeira conta tem " + primeiraConta.saldo);
+    System.out.println("segunda conta tem " + segundaConta.saldo);
 }
 }
