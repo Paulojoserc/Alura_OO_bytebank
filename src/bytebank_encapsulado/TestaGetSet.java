fb83675c2f888b082fdddd19c218534a958c0082
Paulojoserc <pjrc83@gmail.com>
2023-02-21 21:58:34 -0300

    Gettes e Setters de referência

--- src/bytebank_encapsulado/TestaGetSet.java
+++ src/bytebank_encapsulado/TestaGetSet.java
@@ -6,5 +6,22 @@ public static void main(String[] args) {
 	//conta.numero = 1337;
 	conta.setNumero(1337);
 	System.out.println(conta.getNumero());
+	
+	Cliente paulo = new Cliente();
+	//conta.titular = paulo;
+	paulo.setNome("Paulo Silveira");
+	
+	conta.setTitular(paulo);
+	
+	System.out.println(conta.getTitular().getNome());
+	
+	conta.getTitular().setProfissao("programador");
+	//O codígo acima em duas linhas:
+	Cliente titularDaConta = conta.getTitular();
+	titularDaConta.setProfissao("programador");
+	
+	System.out.println(titularDaConta);
+	System.out.println(paulo);
+	System.out.println(conta.getTitular());
 }
 }
