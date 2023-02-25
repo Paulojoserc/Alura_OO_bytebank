88b797d4bb58eaf62b81dec4a861e89c85f3ce00
Paulojoserc <pjrc83@gmail.com>
2023-02-24 23:09:14 -0300

    Super com métodos

--- src/bytebank_herdado/Gerente.java
+++ src/bytebank_herdado/Gerente.java
@@ -19,7 +19,7 @@ public class Gerente extends Funcionario {
 
 	public double getBonificacao() {
 		// return this.getSalario();
-		return super.salario;
+		return super.getBonificacao() + super.getSalario();
 	}
 
 }
