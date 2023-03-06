e5f7532eeea28367f69c2a9cdbb1d382109e36cf
Paulojoserc <pjrc83@gmail.com>
2023-03-05 23:11:30 -0300

    Revendo a composição

--- src/bytebank_herdado/Gerente.java
+++ src/bytebank_herdado/Gerente.java
@@ -3,7 +3,12 @@ package bytebank_herdado;
 //Gerente eh um Funcionario, Gerente herda da class Funcionario, e assina o contrato Autenticavel, eh um Autenticavel
 
 public class Gerente extends Funcionario implements Autenticavel {
-	private int senha;
+
+	private AutenticacaoUtil autenticador;
+
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 
 	public double getBonificacao() {
 		System.out.println("Chamando o método de bonificacao do Gerente");
@@ -11,19 +16,14 @@ public class Gerente extends Funcionario implements Autenticavel {
 		return super.getSalario();
 	}
 
-	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
+		return this.autenticador.autentica(senha);
 
 	}
 
